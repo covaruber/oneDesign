16028ea6d4d52b3dd73b2b577043babab475b7e1
covaruber <cova_ruber@live.com.mx>
2023-06-26 23:00:35 -0600

    initial commit

---
+++ oneDesign.R
@@ -0,0 +1,23 @@
+oneDesign <- function(design, entryList) {
+  
+  ## validate design table and entryList are valid
+  
+  ####################
+  ## ACROSS ENV DESIGN
+  ####################
+  ## take the entry list and based on 'design' input split in the different environments
+  
+  
+  ####################
+  ## WITHIN ENV DESIGN
+  ####################
+  ## take in a for loop each trial and perform the randomization
+  
+  ####################
+  ## RESHAPE AND RETURN
+  ####################
+  ## return the randomization for each trial/instance of the experiment
+  
+  
+  return()
+}
